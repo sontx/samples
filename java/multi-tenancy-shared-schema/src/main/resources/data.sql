0d4e5b0de935ab1d7434a0c366e756f87c99c965
sontx <xuanson33bk@gmail.com>
2023-09-01 20:28:21 +0700

    Update data sample

--- java/multi-tenancy-shared-schema/src/main/resources/data.sql
+++ java/multi-tenancy-shared-schema/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO company (name, address, tenant) VALUES ('Company 1', 'Address 1', 'default');
-INSERT INTO company (name, address, tenant) VALUES ('Company 2', 'Address 2', 'default');
-INSERT INTO company (name, address, tenant) VALUES ('Company 3', 'Address 3', 'default');
-INSERT INTO company (name, address, tenant) VALUES ('Company 4', 'Address 4', 'default');
+INSERT INTO company (name, address, tenant) VALUES ('Company 1', 'Address 1', 'tenant1');
+INSERT INTO company (name, address, tenant) VALUES ('Company 2', 'Address 2', 'tenant1');
+INSERT INTO company (name, address, tenant) VALUES ('Company 3', 'Address 3', 'tenant2');
+INSERT INTO company (name, address, tenant) VALUES ('Company 4', 'Address 4', 'tenant2');
